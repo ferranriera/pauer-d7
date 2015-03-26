b6462a0d2f3d8330968a7d769c1b2beb971c4ce2
friera <frb@ferranriera.com>
2015-03-26 18:40:11 +0100

    Fast commit

--- scripts/pauer_commit.sh
+++ scripts/pauer_commit.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 . assets/pauer_info.sh
 
-drush $alias_drush_from fu-all  -y
+if [ "$fu" == "1" ]; then
+	drush $alias_drush_from fu-all  -y
+fi
+
+
 git checkout master
 git add --all
 git commit -m "Fast commit"
 git push
-ssh $alias_ssh_to -t 'cd www; ./pauer_update.sh fr=1; exit; bash -l'
+
+if [ "$fu" == "1" ]; then
+$alias_ssh_to -t 'cd www; ./pauer_update.sh fr=1 to=$to; exit; bash -l'
+fi
